fb9226a0c62ea8584a3d2f5766d0ba2f31383915
Kamigami55 <eason@easonchang.com>
2022-01-09 02:08:56 +0800

    Solve day02 part2

---
+++ day02/part2.js
@@ -0,0 +1,51 @@
+const lineByLine = require('n-readlines')
+
+const inputFile = 'input.txt'
+// const inputFile = 'sample-input.txt'
+
+function prepareInput() {
+  const liner = new lineByLine(inputFile)
+  let line
+  let actions = []
+  while ((line = liner.next())) {
+    const [direction, distance] = line.toString('utf8').split(' ')
+    actions.push({ direction, distance: parseInt(distance) })
+  }
+  return actions
+}
+
+/**
+ * @param {Array} actions
+ * @returns {Number} ans
+ */
+function solve(actions) {
+  let x = 0
+  let y = 0
+  let aim = 0
+  for (let action of actions) {
+    const { direction, distance } = action
+    switch (direction) {
+      case 'forward':
+        x += distance
+        y += distance * aim
+        break
+      case 'up':
+        aim -= distance
+        break
+      case 'down':
+        aim += distance
+        break
+      default:
+        break
+    }
+  }
+  return x * y
+}
+
+function main() {
+  const actions = prepareInput()
+  const ans = solve(actions)
+  console.log(ans)
+}
+
+main()
